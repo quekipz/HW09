3d6d628284a4bb445fa1397e3e7d5388b1ac3d9d
Kipsy Quevada <quekipz@gmail.com>
2018-07-29 21:30:08 -0400

    Updated for HW10

--- HW09KipsyQuevada.py
+++ HW09KipsyQuevada.py
@@ -46,24 +46,66 @@ def file_reader(path, field_count, sep=',', header=False):
 #*********************
 #CLASSES
 
+#Major Class
+class Major:
+    """ return the summary data about a single major needed in the pretty table """
+
+    def __init__(self, major):
+        """ initialize Student instance """
+        self.major = major
+        self.required_courses = list()
+        self.electives = list()
+    
+    def get_major(self):
+        """ return major name """
+        return self.major
+
+    def add_required_course(self, required):
+        """ add required course from majors.txt """
+        self.required_courses += [required]
+    
+    def add_elective(self, elective):
+        """ add elective from majors.txt """
+        self.electives += [elective]
+
+    def get_required(self):
+        """ get required courses from majors.txt """
+        return self.required_courses
+
+    def get_electives(self):
+        """ get required electives from majors.txt """
+        return self.electives
+
+    def get_major_data(self):
+        """" return major data in prettytable format """
+        return [self.major, sorted(self.required_courses), sorted(self.electives)]
+
 #Student Class
 class Student:
     """ allow other classes to add a course and grade to the container of courses and grades and return the summary data about a single student needed in the pretty table """
 
-    def __init__(self, cwid, name, major):
+    def __init__(self, cwid, name, major, remaining, electives):
         """ initialize Student instance """
         self.cwid = cwid
         self.name = name
         self.major = major
         self.course_grade_dict = {} # each key represents the course name; each value represents the grade for that course
-    
+        self.remaining_list = list(remaining)
+        self.electives_list = list(electives)
+
     def add_course_and_grade(self, course, grade):
         """ add course and grade from grades.txt """
         self.course_grade_dict[course] = grade
+        if grade in ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']:
+            if course in self.remaining_list:
+                self.remaining_list.remove(course)
+            elif course in self.electives_list:
+                self.electives_list = []
+
 
     def get_student_data(self):
         """" return student data in prettytable format """
-        return [self.cwid, self.name, sorted(self.course_grade_dict.keys())]
+        return [self.cwid, self.name, self.major, sorted(self.course_grade_dict.keys()), sorted(self.remaining_list), sorted(self.electives_list)]
 
 
 #Instructor Class
@@ -100,12 +142,28 @@ class Repository:
     def __init__(self, dir_path):
         """ initialize Repository instance """
         self.dir_path = dir_path
+        self.major_dict = {} # major name: major object
         self.student_list = list()
         self.instructor_list = list()
+        self.grade_list = list()
         try:
+            path = self.dir_path + '/' + "majors.txt"
+            for major, flag, course in file_reader(path, 3, sep='\t', header=False):
+                if major not in self.major_dict.keys():
+                    self.major_dict[major] = Major(major)
+                item1 = self.major_dict[major]
+                if flag == 'R':
+                    item1.add_required_course(course)
+                else:
+                    item1.add_elective(course)
+
             path = self.dir_path + '/' + "students.txt"
             for cwid, name, major in file_reader(path, 3, sep='\t', header=False):
-                self.student_list.append(Student(cwid, name, major))
+                if major in self.major_dict.keys():
+                    found_major = self.major_dict[major]
+                    self.student_list.append(Student(cwid, name, major, found_major.get_required(), found_major.get_electives()))
+                else:
+                    raise SyntaxError("No corresponding major found in majors.txt")
             path = self.dir_path + '/' + "instructors.txt"
             for cwid, name, dept in file_reader(path, 3, sep='\t', header=False):
                 self.instructor_list.append(Instructor(cwid, name, dept))
@@ -117,18 +175,34 @@ class Repository:
                 for item2 in self.instructor_list:
                     if i_cwid == item2.cwid:
                         item2.add_course_and_stud(course)
-
         except FileNotFoundError:
             print("The directory", self.dir_path, "does not exist.")
         
+    def create_maj_pretty_tables(self):
+        """ create major pretty table """
+        major_summary = PrettyTable(field_names=["Dept", "Required", "Electives"])
+        for major, major_object in self.major_dict.items():
+            dept, required, electives = major_object.get_major_data()
+            if required == []:
+                required = 'No Required Classes On Record'
+            if electives == []:
+                electives = 'No Electives On Record'
+            major_summary.add_row([dept, required, electives])
+        print ("Major Summary")
+        print(major_summary)
+
     def create_stud_pretty_tables(self):
         """ create student pretty table """
-        student_summary = PrettyTable(field_names=["CWID", "Name", "Completed Courses"])
+        student_summary = PrettyTable(field_names=["CWID", "Name", "Major", "Completed Courses", "Remaining Required", "Remaining Electives"])
         for stud in self.student_list:
-            cwid, name, course_list = stud.get_student_data()
+            cwid, name, major, course_list, remaining_list, electives_list = stud.get_student_data()
             if course_list == []:
                 course_list = 'No Classes On Record'
-            student_summary.add_row([cwid, name, course_list])
+            if remaining_list == []:
+                remaining_list = 'None'
+            if electives_list == []:
+                electives_list = 'None'
+            student_summary.add_row([cwid, name, major, course_list, remaining_list, electives_list])
         print ("Student Summary")
         print(student_summary)
     
@@ -154,11 +228,10 @@ class Repository:
 #PART 1:
 def main():
     # unittest.main()
-    stevens = Repository("/Users/Class2018/Desktop/ssw810/HW09") # read files and generate prettytables
+    stevens = Repository("/Users/Class2018/Desktop/ssw810/GIT College Repository/HW09") # read files and generate prettytables
+    stevens.create_maj_pretty_tables()
     stevens.create_stud_pretty_tables()
     stevens.create_inst_pretty_tables()
 #*********************
 
-
-
-main()  
+# main()
--- HW09Test-KipsyQuevada.py
+++ HW09Test-KipsyQuevada.py
@@ -20,13 +20,23 @@ class RepositoryTest(unittest.TestCase):
         """ test create_stud_pretty_tables function """
         results = list()
         results2 = list()
-        rep = Repository("/Users/Class2018/Desktop/ssw810/HW09")
+        results3 = list()
+        rep = Repository("/Users/Class2018/Desktop/ssw810/GIT College Repository/HW09")
+        for key, maj in rep.major_dict.items():
+            dept, required, electives = maj.get_major_data()
+            results3.append(dept)
+            results3.append(required)
+            results3.append(electives)
+        self.assertEqual(results3, ['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 'SYEN', ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810']])
         for stud in rep.student_list:
-            cwid, name, course_list = stud.get_student_data()
+            cwid, name, major, course_list, remaining_list, electives_list = stud.get_student_data()
             results.append(cwid)
             results.append(name)
+            results.append(major)
             results.append(course_list)
-        self.assertEqual(results, ['10103', "Baldwin, C", ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], '10115', "Wyatt, X", ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], '10172', "Forbes, I", ['SSW 555', 'SSW 567'], '10175', "Erickson, D", ['SSW 564', 'SSW 567', 'SSW 687'], '10183', "Chapman, O", ['SSW 689'], '11399', "Cordova, I", ['SSW 540'], '11461', "Wright, U", ['SYS 611', 'SYS 750', 'SYS 800'], '11658', "Kelly, P", ['SSW 540'], '11714', "Morton, A", ['SYS 611', 'SYS 645'], '11788', "Fuller, E", ['SSW 540']])
+            results.append(remaining_list)
+            results.append(electives_list)
+        self.assertEqual(results, ['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], '10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], '10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], '10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], '10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], '11399', 'Cordova, I', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], '11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], '11658', 'Kelly, P', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], '11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], '11788', 'Fuller, E', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], []])
         for inst in rep.instructor_list:
             for course_tally in inst.get_instructor_data():
                 cwid, name, dept, course, students = course_tally
